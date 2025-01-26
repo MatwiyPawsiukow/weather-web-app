392b0a43017bf826f8ef6c34f2ea2a7a0a887ad0
Matwi2343 <centuriongg2343@gmail.com>
2025-01-26 19:55:58 +0100

    Projekt finished
    js code finish
    
    All work now!!!!1

---
+++ app.js
@@ -0,0 +1,31 @@
+async function getWeather (){
+    const cityInput = document.getElementById("cityInput").value.trim()
+
+    if(!cityInput){
+       document.getElementById("weatherResult").innerHTML = `<p>Error: Please enter a city name.</p>`;
+        return;
+    }
+
+    const apiKey = "57e5e3f9294a7ecbd5ae534a20a8886b";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${apiKey}&units=metric`;
+
+    try{
+        const response = await fetch(url);
+        if(!response.ok) throw new Error("City not found");
+
+        const data = await response.json();
+        const weatherResult = document.getElementById("weatherResult");
+
+        weatherResult.innerHTML = `
+            <h2>${data.name}, ${data.sys.country}</h2>
+            <p>Temperature: ${data.main.temp}°C</p>
+            <p>Weather: ${data.weather[0].description}</p>
+            <p>Humidity: ${data.main.humidity}%</p>
+        `;
+        
+    } catch(error){
+        document.getElementById("weatherResult").innerHTML = `<p>Error: ${error.message}</p>`;
+
+    }
+
+}
